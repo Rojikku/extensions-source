23127391efc1e9dc688b4d8eb9bf6b9c907e97b8
oussama <mrissaoussama@gmail.com>
2025-12-10 19:40:49 +0100

    edit build script

--- src/en/srankmanga/src/eu/kanade/tachiyomi/extension/en/srankm anga/SRankManga.kt
+++
@@ -1,9 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.srankmanga
-
-import eu.kanade.tachiyomi.multisrc.madara.Madara
-
-class SRankManga : Madara(
-    baseUrl = "https://srankmanga.com",
-    name = "S-Rank Manga",
-    lang = "en",
-)
